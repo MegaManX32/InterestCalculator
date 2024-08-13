5589874b5d5274cb7e020a05c1f8100dd44f5705
Vladislav Simovic <vladislav.simovic@acronis.com>
2024-08-13 16:52:33 +0200

    Properly calculate rates

--- src/interestcalculator/calculate/Calculator.java
+++ src/interestcalculator/calculate/Calculator.java
@@ -15,46 +15,46 @@ public class Calculator {
 
     private static Interest[] interests = new Interest[]{
          // 2000 - 2009
-        new Interest(DateCreator.date(1, 1, 2000), DateCreator.date(31, 12, 2009), 6.00),
+        new Interest(DateCreator.date(1, 1, 2000), DateCreator.date(1, 1, 2010), 6.00),
         
         // 2010
-        new Interest(DateCreator.date(1, 1, 2010), DateCreator.date(30, 6, 2010), 8.00),
-        new Interest(DateCreator.date(1, 7, 2010), DateCreator.date(31, 12, 2010), 8.00),
+        new Interest(DateCreator.date(1, 1, 2010), DateCreator.date(1, 7, 2010), 8.00),
+        new Interest(DateCreator.date(1, 7, 2010), DateCreator.date(1, 1, 2011), 8.00),
         
         // 2011
-        new Interest(DateCreator.date(1, 1, 2011), DateCreator.date(30, 6, 2011), 8.00),
-        new Interest(DateCreator.date(1, 7, 2011), DateCreator.date(31, 12, 2011), 8.25),
+        new Interest(DateCreator.date(1, 1, 2011), DateCreator.date(1, 7, 2011), 8.00),
+        new Interest(DateCreator.date(1, 7, 2011), DateCreator.date(1, 1, 2012), 8.25),
         
         // 2012
-        new Interest(DateCreator.date(1, 1, 2012), DateCreator.date(30, 6, 2012), 8.00),
-        new Interest(DateCreator.date(1, 7, 2012), DateCreator.date(31, 12, 2012), 8.00),
+        new Interest(DateCreator.date(1, 1, 2012), DateCreator.date(1, 7, 2012), 8.00),
+        new Interest(DateCreator.date(1, 7, 2012), DateCreator.date(1, 1, 2013), 8.00),
         
         // 2013
-        new Interest(DateCreator.date(1, 1, 2013), DateCreator.date(30, 6, 2013), 7.75),
-        new Interest(DateCreator.date(1, 7, 2013), DateCreator.date(31, 12, 2013), 7.50),
+        new Interest(DateCreator.date(1, 1, 2013), DateCreator.date(1, 7, 2013), 7.75),
+        new Interest(DateCreator.date(1, 7, 2013), DateCreator.date(1, 1, 2014), 7.50),
         
         // 2014
-        new Interest(DateCreator.date(1, 1, 2014), DateCreator.date(30, 6, 2014), 7.25),
-        new Interest(DateCreator.date(1, 7, 2014), DateCreator.date(31, 12, 2014), 7.15),
+        new Interest(DateCreator.date(1, 1, 2014), DateCreator.date(1, 7, 2014), 7.25),
+        new Interest(DateCreator.date(1, 7, 2014), DateCreator.date(1, 1, 2015), 7.15),
         
         // 2015
-        new Interest(DateCreator.date(1, 1, 2015), DateCreator.date(30, 6, 2015), 7.05),
-        new Interest(DateCreator.date(1, 7, 2015), DateCreator.date(31, 12, 2015), 7.05),
+        new Interest(DateCreator.date(1, 1, 2015), DateCreator.date(1, 7, 2015), 7.05),
+        new Interest(DateCreator.date(1, 7, 2015), DateCreator.date(1, 1, 2016), 7.05),
         
         // 2016 - 2018
-        new Interest(DateCreator.date(1, 1, 2016), DateCreator.date(30, 6, 2016), 7.05),
-        new Interest(DateCreator.date(1, 7, 2016), DateCreator.date(31, 12, 2018), 7.00),
+        new Interest(DateCreator.date(1, 1, 2016), DateCreator.date(1, 7, 2016), 7.05),
+        new Interest(DateCreator.date(1, 7, 2016), DateCreator.date(1, 1, 2019), 7.00),
         
         // 2019 - 2022
-        new Interest(DateCreator.date(1, 1, 2019), DateCreator.date(31, 12, 2022), 8.00),
+        new Interest(DateCreator.date(1, 1, 2019), DateCreator.date(1, 1, 2023), 8.00),
         
         // 2023
-        new Interest(DateCreator.date(1, 1, 2023), DateCreator.date(30, 6, 2023), 10.50),
-        new Interest(DateCreator.date(1, 7, 2023), DateCreator.date(31, 12, 2023), 12.00),
+        new Interest(DateCreator.date(1, 1, 2023), DateCreator.date(1, 7, 2023), 10.50),
+        new Interest(DateCreator.date(1, 7, 2023), DateCreator.date(1, 1, 2024), 12.00),
         
         // 2024
-        new Interest(DateCreator.date(1, 1, 2024), DateCreator.date(30, 6, 2024), 12.50),
-        new Interest(DateCreator.date(1, 7, 2024), DateCreator.date(31, 12, 2024), 12.25)
+        new Interest(DateCreator.date(1, 1, 2024), DateCreator.date(1, 7, 2024), 12.50),
+        new Interest(DateCreator.date(1, 7, 2024), DateCreator.date(1, 1, 2025), 12.25)
     };
 
     public String generate(LocalDate start, LocalDate end, double amount) {
@@ -71,9 +71,11 @@ public class Calculator {
             if (minDate.isAfter(maxDate)) continue;
             
             double daysBetween = ChronoUnit.DAYS.between(minDate, maxDate);
-            result += daysBetween * interest.getValue() * amount / 100;
+            System.out.println("" + daysBetween);
+            
+            result += daysBetween * interest.getValue() * amount;
         }
         
-        return "Interest: " + result;
+        return "Interest: " + result; /// 36.500;
     }
 }
